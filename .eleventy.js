687b3f5c5b17a7eb4584f168fe44a07da20a3743
Mathis Thomsen <matthomster@gmail.com>
2021-06-27 00:40:06 +0200

    Logic
    Added more modular approach with includes, more logic and automation in rendering.
    TODO: Read all Link after Category on home, menu

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,17 @@ module.exports = function(eleventyConfig){
     eleventyConfig.addPassthroughCopy('./src/style.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
 
+    eleventyConfig.addCollection('tagsList', (collectionApi) => {
+        const tagsSet = new Set()
+        collectionApi.getAll().forEach((item) => {
+          if (!item.data.tags) return
+          item.data.tags
+            .filter((tag) => !['all', 'featured', 'article'].includes(tag))
+            .forEach((tag) => tagsSet.add(tag))
+        })
+        return [...tagsSet].sort((a, b) => b.localeCompare(a))
+      })
+
     return {
         dir: {
             input: "src",
---
+++ src/articles/2021-06-26-stop-searching-for-you.md
@@ -0,0 +1,11 @@
+---
+title: Stop searching for you
+author: Mathis Thomsen
+tags:
+    - meta
+    - article
+category: meta
+description: Was ist so besonders an dir, dass du glaubst es lohne sich gefunden zu werden?
+image: image-11.jpg
+---
+Ich bin dann mal auf einem Selbsfindungstrip. Ich muss mich selbst finden. Ich weiß nicht, wer ich bin. Unsere Zeit legt ungeheur viel Wert darauf, dass jeder weiß, wer er ist. Dabei ist das nun wirklich nicht so schwer. Und es gibt viel wichtigere Fragen zu beantworten. Zieht euren Kopf aus dem Allerwertesten und kümmert euch um die bedeutenden Dinge.
